c98f66b25e26bc40a5ce3f68eb4b035699862f25
adamreaksmey <adaminiature@gmail.com>
2024-06-13 18:54:58 +0700

    init main function

---
+++ main.js
@@ -0,0 +1,9 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const main = async (__filename, __dirname) => {};
+
+main(__filename, __dirname).catch(console.error);
