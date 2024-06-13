ca58ebad49941e819d3f6006e832dd01d1350a84
adamreaksmey <adaminiature@gmail.com>
2024-06-13 22:19:12 +0700

    moved utilities functions

---
+++ src/functions/functions.js
@@ -0,0 +1,26 @@
+/**
+ * Checks if value is valid uuid
+ * @param {*} value
+ * @returns
+ */
+export const isUUID = (value) => {
+  const uuidPattern =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  const isUUID = uuidPattern.test(value);
+  if (isUUID) return value;
+  //   return "b740450d-a05d-4e1d-a235-1d507702f30d";
+};
+
+/**
+ * Mapper to use to compensate for the find function.
+ * @param {*} data
+ * @param {*} key
+ * @returns
+ */
+export const newMapper = (data, key) => {
+  return new Map(
+    data.map((user) => {
+      return [user[key], user];
+    })
+  );
+};
--- src/services/lms-service/functions/operations/functions.js
+++ src/services/lms-service/functions/operations/functions.js
@@ -1,17 +1,4 @@
 /**
- * Checks if value is valid uuid
- * @param {*} value
- * @returns
- */
-export const isUUID = (value) => {
-  const uuidPattern =
-    /^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-  const isUUID = uuidPattern.test(value);
-  if (isUUID) return value;
-  //   return "b740450d-a05d-4e1d-a235-1d507702f30d";
-};
-
-/**
  * Recursive function to get lesson children
  * @param {*} lessons
  * @param {*} ids
@@ -86,18 +73,4 @@ export const searchDelete = (tree, idToDelete) => {
     }
   }
   return cleanTree;
-};
-
-/**
- * Mapper to use to compensate for the find function.
- * @param {*} data
- * @param {*} key
- * @returns
- */
-export const newMapper = (data, key) => {
-  return new Map(
-    data.map((user) => {
-      return [user[key], user];
-    })
-  );
 };
