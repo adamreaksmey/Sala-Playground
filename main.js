ecbcdcbe7a1a80918848d052200b803cb664048c
adamreaksmey <adaminiature@gmail.com>
2024-06-13 18:57:19 +0700

    added served data

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const main = async (__filename, __dirname) => {};
+const main = async (__filename, __dirname) => {
+};
 
 main(__filename, __dirname).catch(console.error);
