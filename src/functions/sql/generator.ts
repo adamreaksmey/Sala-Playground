5eaa2ef067c7d4ad3c437eb39ba001b28e6c7d8d
adamreaksmey <adaminiature@gmail.com>
2024-06-17 10:11:41 +0700

    added tableConfig and their settings

--- src/functions/sql/generator.ts
+++ src/functions/sql/generator.ts
@@ -6,13 +6,37 @@ import { dirname, join } from 'path'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+interface TableEntityConfig {
+  updateColumns: string[] /* Defines what columns you want updated. */
+  idColumn: string /* Your key column, this can be used to find a specific row from a value. */
+}
+
+interface TableConfig {
+  /* The key is your table's name or tableName's value. */
+  [key: string]: TableEntityConfig
+}
+
+/**
+ * Current setting example. modify as you desire! :)
+ */
+const tableConfig: TableConfig = {
+  user: {
+    updateColumns: ['guardianId', 'guardianName', 'employer'],
+    idColumn: 'idCard',
+  },
+  student: {
+    updateColumns: ['groupStructureId', 'structureRecordId'],
+    idColumn: 'idCard',
+  },
+}
+
 class SQLgenerator {
   constructor() {}
 
   sayHello(): any {
     console.log('hello world')
   }
-  
+
   /**
    * Write generated inserts statement to file.
    * if path exists, it writes into there. if no,
