2faa6f45f92670d32d7dee6f7bec1e071bed1a93
adamreaksmey <adaminiature@gmail.com>
2024-06-25 12:14:47 +0700

    added endpoints

---
+++ src/services/product-service/api/endpoints.ts
@@ -0,0 +1,8 @@
+/* Local & dev url */
+export const localHost = 'http://localhost:8080/products_service'
+export const devHost = 'https://sms-api.dev.sala.tech/products_service'
+/* Staging url */
+export const IBF_stagingHost = 'https://sms-api.staging.ibfkh.org/products_service'
+export const SALA_stagingHost = 'https://sms-api.staging.sala.tech/products_service'
+/* Production url */
+export const IBF_prodHost = 'https://sms-api.ibfkh.org/products_service'
