c77be4ddf15aa012e4184455b6f503e2def8babd
adamreaksmey <adaminiature@gmail.com>
2024-06-25 13:02:53 +0700

    cleaned up main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,13 @@
-import {
-  createOrganization,
-  updatedOrganization,
-} from './services/product-service/api/api'
-import { v4 as uuidv4 } from 'uuid'
-import _File from './functions/files/functions'
-import created_uuid from './services/product-service/logs/created_uuid'
-import { generateRandomShits } from './functions/functions'
-
+/**
+ * This is where main functions are performed.
+ * You may read the documents in the notes, import
+ * and call functions from services, functions, ...etc.
+ *
+ * and perform your desired tasks. Happy coding! :)
+ */
 type MainFunctionType = () => Promise<void>
 const main: MainFunctionType = async () => {
-  const storedUUId = []
-
-  // for (let i = 0; i < 100; i++) {
-  //   const payload = {
-  //     referenceId: uuidv4(),
-  //     name: 'IBF Institute' + i,
-  //     nameNative: 'IBF Institute native' + i,
-  //     code: 'ORG-CODE' + i,
-  //     address: 'street 12 #12, Phnom Penh',
-  //     description: 'Some description' + i,
-  //     bankInfo: [
-  //       {
-  //         bankName: '',
-  //         bankNumber: '',
-  //         bankImage: [],
-  //         bankCurrency: '$',
-  //       },
-  //     ],
-  //   }
-  //   storedUUId.push(payload.referenceId)
-  //   const response = await createOrganization(payload)
-  //   console.log('success response', response?.data)
-  // }
-
-  // _File.reWriter(
-  //   'src/services/product-service/logs/created_uuid.ts',
-  //   `export default ${JSON.stringify(storedUUId)}`
-  // )
-
-  for (const iterator of created_uuid) {
-    const payload = {
-      orgId: iterator,
-      name: generateRandomShits(10),
-      nameNative: generateRandomShits(10),
-      code: generateRandomShits(5),
-    }
-
-    const response = await updatedOrganization(payload)
-    console.log(response?.data)
-  }
+  // Perform magic here :)
 }
 
 main().catch(console.error)
