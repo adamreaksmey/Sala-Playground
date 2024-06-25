b54844f82e2a6d02ad90a9ea5e9c03daf7903f66
adamreaksmey <adaminiature@gmail.com>
2024-06-25 12:30:21 +0700

    installed uuid generator

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createOrganization } from './services/product-service/api/api'
+import { v4 as uuidv4 } from 'uuid';
 
 type MainFunctionType = () => Promise<void>
 const main: MainFunctionType = async () => {
   for (let i = 0; i < 100; i++) {
     const payload = {
+      referenceId: '{{ORG_ID}}',
       name: 'IBF Institute' + i,
       nameNative: 'IBF Institute native' + i,
       code: 'ORG-CODE' + i,
