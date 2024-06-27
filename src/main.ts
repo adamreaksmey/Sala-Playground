f1bb8bdad476b9d7712ab7b4be3d29212e1c4fcb
adamreaksmey <adaminiature@gmail.com>
2024-06-27 19:32:25 +0700

    added commants back

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import dotenv from 'dotenv'
 dotenv.config()
-
-import Https from './functions/http/http'
+/**
+ * This is where main functions are performed.
+ * You may read the documents in the notes, import
+ * and call functions from services, functions, ...etc.
+ *
+ * and perform your desired tasks. Happy coding! :)
+ */
 type MainFunctionType = () => Promise<void>
 const main: MainFunctionType = async () => {
   
