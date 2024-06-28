52ee6137e8dca511999ae0ee457c6ff81decca13
adamreaksmey <adaminiature@gmail.com>
2024-06-28 13:52:44 +0700

    added head and put

--- src/functions/http/http.ts
+++ src/functions/http/http.ts
@@ -53,6 +53,28 @@ class Https implements __HTTP__ {
 
     return response
   }
+
+  public async _head(url: string, payload?: any): Promise<AxiosResponse<any, any>> {
+    const response: AxiosResponse<any, any> = await axios({
+      method: 'HEAD',
+      baseURL: this.host,
+      url,
+      data: payload,
+    })
+
+    return response
+  }
+
+  public async _put(url: string, payload?: any): Promise<AxiosResponse<any, any>> {
+    const response: AxiosResponse<any, any> = await axios({
+      method: 'PUT',
+      baseURL: this.host,
+      url,
+      data: payload,
+    })
+
+    return response.data
+  }
 }
 
 export default Https
