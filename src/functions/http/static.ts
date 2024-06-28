0b1b4f194831be8bb527c107f0ace27d5daef753
adamreaksmey <adaminiature@gmail.com>
2024-06-28 14:00:44 +0700

    reorgnization http methods

--- src/functions/http/static.ts
+++ src/functions/http/static.ts
@@ -2,21 +2,24 @@ import type { AxiosResponse } from 'axios'
 /**
  * Reserved http methods
  */
-export const httpMethods = {
-  post: 'POST',
-  get: 'GET',
-  patch: 'PATCH',
-  delete: 'DELETE',
+export enum HttpMethods {
+  GET = 'get',
+  POST = 'post',
+  PATCH = 'patch',
+  PUT = 'put',
+  CONNECT = 'connect',
+  OPTIONS = 'options',
+  TRACE = 'trace',
+  HEAD = 'head',
 }
 
 export interface __HTTP__ {
-  _get(url: string, payload: any): Promise<AxiosResponse<any, any>>
-  _post(url: string, payload: any): Promise<AxiosResponse<any, any>>
-  _patch(url: string, payload: any): Promise<AxiosResponse<any, any>>
-  _delete(url: string, payload: any): Promise<AxiosResponse<any, any>>
-  _head(url: string, payload?: any): Promise<AxiosResponse<any, any>>
+  _get(url: string, payload?: any): Promise<AxiosResponse<any, any>>
+  _post(url: string, payload?: any): Promise<AxiosResponse<any, any>>
+  _patch(url: string, payload?: any): Promise<AxiosResponse<any, any>>
   _put(url: string, payload?: any): Promise<AxiosResponse<any, any>>
   _connect(url: string, payload?: any): Promise<AxiosResponse<any, any>>
   _options(url: string, payload?: any): Promise<AxiosResponse<any, any>>
   _trace(url: string, payload?: any): Promise<AxiosResponse<any, any>>
+  _head(url: string, payload?: any): Promise<AxiosResponse<any, any>>
 }
