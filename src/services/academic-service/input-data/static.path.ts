dc90372724dbfffcac23a9146058f448b9c0c635
adamreaksmey <adaminiature@gmail.com>
2024-07-01 15:26:39 +0700

    added static path

---
+++ src/services/academic-service/input-data/static.path.ts
@@ -0,0 +1,28 @@
+export const paths = [
+  {
+    academicService: [
+      {
+        students:
+          '/Users/michaellogy/Desktop/sala-projects/data-sync-methods/students.csv',
+      },
+      {
+        school: '/Users/michaellogy/Desktop/sala-projects/data-sync-methods/school.csv',
+      },
+      {
+        configuration:
+          '/Users/michaellogy/Desktop/sala-projects/data-sync-methods/configuration.csv',
+      },
+      {
+        student_schema:
+          '/Users/michaellogy/Desktop/sala-projects/data-sync-methods/student_schema.csv',
+      },
+    ],
+  },
+  {
+    applicantService: [
+      {
+        applicants: {},
+      },
+    ],
+  },
+]
