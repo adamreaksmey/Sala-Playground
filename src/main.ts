32ae90f28cba12fbc3f2b8c83cd486ca34b2a31d
adamreaksmey <adaminiature@gmail.com>
2024-07-01 15:23:33 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config()
+import { csvToJson } from './services/academic-service/functions/csvtojson'
 /**
  * This is where main functions are performed.
  * You may read the documents in the notes, import
@@ -9,7 +10,39 @@ dotenv.config()
  */
 type MainFunctionType = () => Promise<void>
 const main: MainFunctionType = async () => {
-  
+  const paths = [
+    {
+      academicService: [
+        {
+          students:
+            '/Users/michaellogy/Desktop/sala-projects/data-sync-methods/students.csv',
+        },
+        {
+          school: '/Users/michaellogy/Desktop/sala-projects/data-sync-methods/school.csv',
+        },
+        {
+          configuration:
+            '/Users/michaellogy/Desktop/sala-projects/data-sync-methods/configuration.csv',
+        },
+        {
+          student_schema:
+            '/Users/michaellogy/Desktop/sala-projects/data-sync-methods/student_schema.csv',
+        },
+      ],
+    },
+    {
+      applicantService: [
+        {
+          applicants: {},
+        },
+      ],
+    },
+  ]
+
+  const response = await csvToJson(
+    '/Users/michaellogy/Desktop/sala-projects/data-sync-methods/configuration.csv'
+  )
+  console.log(response)
 }
 
 main().catch(console.error)
--- src/services/academic-service/functions/csvtojson.ts
+++ src/services/academic-service/functions/csvtojson.ts
@@ -3,16 +3,23 @@ import FormData from 'form-data'
 import fs from 'fs'
 import Https from '../../../functions/http/http'
 
-const csvToJsonUrl = 'https://www.convertcsv.io'
-const path = '/api/v1/csv2json/'
-// Replace with your file path and token
-const filePath: string = '[YOUR FILE]'
-const token: string = '[YOUR TOKEN]'
+export const csvToJson = async (filePath: string) => {
+  const csvToJsonUrl = 'https://www.convertcsv.io'
+  const path = '/api/v1/csv2json/'
+  // Replace with your file path and token
+  const token: string = '5498a17787c67a0ecd8e17650af12b11b705f5d2'
 
-// Initializing formData
-const formData = new FormData()
-formData.append('infile', fs.createReadStream(filePath))
+  // Initializing formData
+  const formData: FormData = new FormData()
+  formData.append('infile', fs.createReadStream(filePath))
+  const headers = {
+    Authorization: `Token ${token}`,
+    ...formData.getHeaders(),
+  }
 
-// Perform the POST request using Axios
-const instance = new Https(csvToJsonUrl)
-const response = await instance._post(path, formData)
+  // Perform the POST request using Axios
+  const instance = new Https(csvToJsonUrl)
+  const response = await instance._post(path, formData, headers)
+
+  return response.data
+}
