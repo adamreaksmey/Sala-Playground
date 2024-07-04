56297a9006edcf81fe42aa8f41b6a39f4780f5a5
adamreaksmey <adaminiature@gmail.com>
2024-07-04 22:50:32 +0700

    init guardian dummy data

--- src/main.ts
+++ src/main.ts
@@ -14,37 +14,29 @@ const main: MainFunctionType = async () => {
 
   const counterSet: number = 150
   for (let i = 0; i < counterSet; i++) {
-    const articlePayload = {
-      slug: faker.lorem.slug(),
-      title: faker.hacker.phrase(),
-      titleNative: faker.hacker.phrase(),
-      description: faker.finance.transactionDescription() || faker.commerce.productDescription(),
-      descriptionNative: faker.finance.transactionDescription() || faker.commerce.productDescription(),
-      author: faker.person.firstName(authorName[randomIndexBasedOnArray(authorName)]),
-      authorAvatar: `https://randomuser.me/api/portraits/men/${randomNumberInRange}.jpg`,
-      minutesRead: randomNumberInRange.toFixed(0),
-      type: 'news',
-      content: faker.lorem.paragraphs(10),
-      contentNative: faker.lorem.paragraphs(10),
-      subtitle: faker.lorem.sentence(),
-      subtitleNative: faker.lorem.sentence(),
-      publishedDate: '2024-07-04T14:48:31.350Z',
-      status: 'active',
-      startDate: faker.date.future().toISOString(),
-      endDate: faker.date.future().toISOString(),
-      metaTitle: faker.lorem.words(3),
-      metaDescription: faker.lorem.sentence(),
-      metaKeyword: faker.lorem.words(3).split(' ').join(', '),
-      tags: faker.lorem.words(5).split(' '),
-      featureImageLink: faker.image.avatar(),
-      featureImage: faker.image.avatar(),
+    const guardianPayload = {
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      firstNameNative: faker.name.firstName(),
+      lastNameNative: faker.name.lastName(),
+      gender: faker.name.gender(), // returns gender string
+      dob: faker.date.past(50, new Date()).toISOString(), // date of birth in the past 50 years
+      phone: faker.phone.phoneNumber(),
+      email: faker.internet.email(),
+      address: faker.address.streetAddress(),
+      photo: faker.image.avatar(),
+      userName: faker.internet.userName(),
+      systemAccess: {
+        role: 'Guardian',
+        password: faker.internet.password(10),
+      },
     }
 
     console.log(i)
 
     const articleCreation = await httpInstance._post(
-      `/news_service/organizations/${pseOrgId}/articles`,
-      articlePayload
+      `/academic_service/schools/${pseOrgId}/guardians`,
+      guardianPayload
     )
 
     console.log(articleCreation.data)
