0fafa9ce1334799d4729efa83c1a78c02baddc77
adamreaksmey <adaminiature@gmail.com>
2024-08-15 16:22:38 +0700

    init stress test

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,39 @@
 import dotenv from 'dotenv'
+import axios from 'axios'
+import path, { dirname } from 'path'
+import * as fs from 'fs'
+import FormData from 'form-data' // Import FormData from the form-data package
+import { fileURLToPath } from 'url'
+
 dotenv.config()
-/**
- * This is where main functions are performed.
- * You may read the documents in the notes, import
- * and call functions from services, functions, ...etc.
- *
- * and perform your desired tasks. Happy coding! :)
- */
+
 type MainFunctionType = () => Promise<void>
+const ORGID = '27c0b377-f28e-4ba1-afb9-5eaed02fee6c'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const formData = new FormData()
+formData.append('name', '::testing::')
+formData.append('extension', 'png')
+formData.append('parent_id', ORGID)
+
+const filePath = path.join(__dirname, '../ramanujan.png')
+formData.append('file', fs.createReadStream(filePath))
+
 const main: MainFunctionType = async () => {
-  
+  try {
+    const response = await axios({
+      baseURL: 'http://127.0.0.1:8000',
+      url: `/api/products_service/${ORGID}/upload`,
+      method: 'POST',
+      data: formData,
+      headers: formData.getHeaders(),
+    })
+
+    console.log('response', response.data) // Log the response data
+  } catch (error) {
+    console.error('Error uploading file', error)
+  }
 }
 
 main().catch(console.error)
---
+++ src/services/file-manager/headers.ts
@@ -0,0 +1,14 @@
+export const headers = {
+  accept: 'application/json, text/plain, */*',
+  'accept-language': 'en-US,en;q=0.9',
+  'content-type': 'multipart/form-data; boundary=----WebKitFormBoundaryFQfNtYPsPY023c3C',
+  'sec-ch-ua': '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': '"macOS"',
+  'sec-fetch-dest': 'empty',
+  'sec-fetch-mode': 'cors',
+  'sec-fetch-site': 'same-origin',
+  'x-requested-with': 'XMLHttpRequest',
+  'x-xsrf-token':
+    'eyJpdiI6Im52VUNoWlhOVTdtdlpXNTAwakpzR1E9PSIsInZhbHVlIjoiSXdGcmFSMTc4dlFCYXQrcjZmRmlYeW11QmMxN3E2REh2dWtZMkMvQWF3STF0MUgvZDlub0E5YktJNVN2WWhkZ2hrRjJMWHNTRU5EeVg4c01RWkhjd2F0ckhqZklYRlg5S002bWMwT0FyVmFLOG9DOUpwK0lzVzM0Z3RYZDhjWmwiLCJtYWMiOiJjYzNmODQwNWRmYmJjM2MwOWRiNDU2MGJiYjk4OTVmY2Y3ZGNkMGZiNDM2YTgzMDRjNzJiYjhhNTEyYjg0OTBkIiwidGFnIjoiIn0=',
+}
